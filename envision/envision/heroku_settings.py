f53356d973599541623dcb09a1837c05600552d6
Tripp <tripp.shealy@gmail.com>
2016-07-09 22:24:56 -0400

    changed heroku settings back to staticfile

--- envision/envision/heroku_settings.py
+++ envision/envision/heroku_settings.py
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
